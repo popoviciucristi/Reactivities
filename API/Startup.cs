03888bcace55cc6226bb66820b3e5b9f53c13c19
Cristian Popoviciu <popoviciucristi001@yahoo.com>
2022-07-14 11:22:24 +0300

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,8 +35,15 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddCors(opt =>{
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -53,7 +60,9 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            
+            app.UseCors("CorsPolicy");
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
